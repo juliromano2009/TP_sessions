883b7e400aa113b98b1bbda5bc9edd36c43eccd6
EMANUEL EITAN MIKLASKI <49628417@ort>
2026-08-13 08:57:16 -0300

    controller actualizado

--- Controllers/SessionController.cs
+++ Controllers/SessionController.cs
@@ -16,4 +16,29 @@ public class SessionController : Controller
     {
         return View();
     }
+
+    //haceme un iactionresult que me permita iniciar sesion, chequee que el usuario exista y que la contraseña sea correcta, si es asi me 
+    // lleve a la pagina de inicio, si nom existe el usuario o la contraseña es incorrecta que me devuelva 
+    // a la pagina de inicio de sesion con un mensaje de error
+    [HttpPost]
+    public IActionResult IniciarSesion(string nombreUsuario, string contraseña)
+    {
+        BD bd = new BD();
+        Usuario usuario = bd.ObtenerUsuario(nombreUsuario, contraseña);
+
+        if (usuario != null)
+        {
+            // El usuario existe y la contraseña es correcta
+            // Redirigir a la página de inicio
+            return ("Bienvenida");
+        }
+        else
+        {
+            // El usuario no existe o la contraseña es incorrecta
+            // Devolver a la página de inicio de sesión con un mensaje de error
+            ViewBag.ErrorMessage = "Nombre de usuario o contraseña incorrectos.";
+            return View("Index");
+        }
+    }
+
 }
